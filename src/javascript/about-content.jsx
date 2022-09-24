4820c2ad0d458585b27afaffa016a8712b57d747
Johan Ospina <johanos@bu.edu>
2022-09-24 18:55:40 -0400

    feat: Update About Blurb

--- src/javascript/about-content.jsx
+++ src/javascript/about-content.jsx
@@ -42,16 +42,48 @@ class AboutContent extends Component {
                     </button>
                 </div>
                 <div className="portfolio-about-content-bio">
-                    Currently, I&apos;m a graduate student at Princeton
-                    University where I&apos;m learning more about Machine
-                    Learning, Computer Vision, and Graphics. Before this I was
-                    building awesome experiences at Wayfair&apos;s Research And
-                    Development Team Wayfair Next which I joined after
-                    graduating Magna Cum Laude from Boston University in 2017
+                    Currently, I&apos;m a Research & Development Engineer III at
+                    <a
+                        href="https://www.markforged.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {' '}
+                        Markforged
+                    </a>
+                    . I came to my current role from
+                    <a
+                        href="https://www.ptc.com/en/technologies/augmented-reality"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {' '}
+                        PTC - Vuforia{' '}
+                    </a>
+                    where I was a Senior Engineer who worked on AR/VR and
+                    Computer Vision technologies such as the Vuforia Model
+                    Target Generator. Before that, I got my first job building
+                    awesome experiences at
+                    <a
+                        href="https://www.aboutwayfair.com/company-news/wayfair-announces-opening-of-research-and-development-laboratory"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        {' '}
+                        Wayfair&apos;s{' '}
+                    </a>
+                    Research And Development Team.
                     <br />
-                    <br />I am looking for opportunities that will allow me to
-                    grow as a researcher, engineer, and creator so I can make
-                    cool stuff happen.
+                    <br />
+                    I hold a Master&apos;s of Science in Computer Science from
+                    Princeton University and a Bachelor&apos;s of Science from
+                    Boston University (Magna Cum Laude). My domain expertise
+                    lies in 3D Graphics, Machine Learning, Computer Vision, and
+                    (of course) Software Engineering.
+                    <br />
+                    <br />I am always looking out for opportunities that will
+                    allow me to grow as a engineer, creator, and researcher so I
+                    can make cool stuff happen.
                 </div>
             </div>
         );
